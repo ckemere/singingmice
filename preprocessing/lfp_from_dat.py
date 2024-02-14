ec11717ef70c93f68d4b236b800dd636bf21c319
Caleb Kemere <ckemere@gmail.com>
2024-02-14 14:50:15 -0500

    Updated lfp_from_dat.py - now we just use a simple filter rather than trying to polyphase

--- preprocessing/lfp_from_dat.py
+++ preprocessing/lfp_from_dat.py
@@ -9,17 +9,18 @@ from pstats import SortKey, Stats
 
 # Step 1: find files in current directory to process
 
-raw_filename = "amplifier.dat" # perhaps should look somewhere else?
+raw_filename = "amplifier.dat" # perhaps should look somewhere else? TODO: Specify path
 
 n_channels = 128 # should infer from info.rhd
 
 ## TODO:bz_LFPfromDat also creates a session file
+_, session_dir  = os.path.split(os.getcwd()) # Assumes we are running from the data directory. TODO: FRAGILE
 
 # Step 2: figure out dimenions of data file
 raw_file_stats = os.stat(raw_filename)
 n_samples = int(raw_file_stats.st_size / 2 / n_channels) # each sample is 2 bytes
 
-# Step 3:
+# Step 3: Filter and downsample
 
 fs = 30000
 low_pass_fs = 450
@@ -31,55 +32,26 @@ filter = np.sinc(np.arange(-int(filter_length/2), int(filter_length/2)+1)*(low_p
 
 ds = int(fs/lfp_fs) # should warn if not an int!
 
-polyphase_filters = []
-for idx in range(ds):
-   if (idx == 0):
-      polyphase_filters.append(filter[::ds])
-   else:
-      polyphase_filters.append(filter[(ds-idx)::ds])
+indata = np.memmap(raw_filename, dtype='uint16', mode = 'r', shape=(n_samples, n_channels), order='C')
 
-# input = np.memmap(raw_filename, dtype='uint16', mode = 'r', shape=(n_samples, n_channels), order='C')
+output_shape, output_dtype = gsp.filter_data_fir(indata, filter, axis=0, describe_dims=True, ds=ds)
 
-# We're going to do something cool. Assuming n_channels is 127, the input data is laid out as
-#      [ch0,sample0] [ch1,sample0] ... [ch127,sample0] [ch0,sample1], [ch1,sample1], etc
-# Because of this, we can implement the input decimation part of the polyphase filter by reshaping
-# the memmap to be {ds} times wider. So then when we apply the filters, we apply them in sequence to the first
-# ds "channels" then the second ds "channels" and so on. In order to have this memmap be smaller than the
-# actual filesize, we'll have to truncate it by a fraction of {ds} samples, meaning that the last few samples
-# of our output may be corrupted a bit...
+output_filename = session_dir + '_' + 'lfp.dat'
 
-# with Profile() as profile:
-indata = np.memmap(raw_filename, dtype='uint16', mode = 'r', shape=(int(n_samples/24), n_channels*24), order='C')
+# The original plan was to use a polyphase filter, but for shorter files its just safer/easier
+# to do this. On my M2 MBAir, we process about 3.25 GB/minute (which corresponds to about 7 minutes of recording).
 
-print(indata.shape)
+outdata = np.memmap(output_filename, dtype='uint16', mode = 'w+', shape=output_shape, order='C')
+print("Output to: ", output_filename)
+
+
 t0= time.time()
-
-filter_inds = np.arange(n_channels, dtype=int)
-output = gsp.filter_data_fir(indata, polyphase_filters[0], axis=0, input_dim_restrictions=[None,filter_inds.tolist()])
+output = gsp.filter_data_fir(indata, filter, axis=0, ds=ds, outarray=outdata)
 t1= time.time()
 print(t1 - t0, 's')
 
-
-for idx in range(ds-1):
-   filter_inds = filter_inds + 128
-   temp = gsp.filter_data_fir(indata, polyphase_filters[idx+1], axis=0, input_dim_restrictions=[None,filter_inds.tolist()])
-   t1= time.time()
-   print(t1 - t0, 's')
-   output[1:,:] += temp[:(output.shape[0]-1),:]
-   t1= time.time()
-   print(t1 - t0, 's')
-
-print(output.shape)
-
-indata = np.memmap(raw_filename, dtype='uint16', mode = 'r', shape=(n_samples, n_channels), order='C')
-
-
-t0= time.time()
-
-output = gsp.filter_data_fir(indata, filter, axis=0)
-print(output.shape)
-
-
+print('Flushing data to disk.')
+outdata.flush()
 t1= time.time()
 print(t1 - t0, 's')
 
